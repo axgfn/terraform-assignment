f990716ae07fcff3f6c3a98bc05dc075cae4857c
Alex Griffin <a@ajgrf.com>
2024-10-03 13:29:47 -0700

    Got it working

--- module.tf
+++ module.tf
@@ -1,11 +1,13 @@
 provider "azurerm" {
-      features {}
+  features {}
+
+  subscription_id = "5683e6ec-188f-49d3-a953-b5878fad3267"
 }
 
 module "sub1" {
-  source = "./subnet"
+  source      = "./subnet"
   subnet_name = "sub1"
   subnet_CIDR = "10.0.1.0/24"
-  rg_name = "rg01-b1"
-  vnet-name = "example-network"
+  rg_name     = "rg01-b1"
+  vnet-name   = "example-network"
 }
--- subnet/Subnet-main.tf
+++ subnet/Subnet-main.tf
@@ -8,6 +8,16 @@ resource "azurerm_subnet" "example" {
   resource_group_name  = data.azurerm_resource_group.rg01.name
   virtual_network_name = var.vnet-name
   address_prefixes     = [var.subnet_CIDR]
+  depends_on = [
+    azurerm_virtual_network.example
+  ]
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet-name
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg01.location
+  resource_group_name  = data.azurerm_resource_group.rg01.name
 }
 
 output "sub_id" {
